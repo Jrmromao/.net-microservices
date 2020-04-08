3124b351eb6af1adabeabe7de527532a2c4b9b2d
Joao Romao <jrmromao@gmail.com>
2020-04-08 23:42:52 +0100

    Implemented Domain project

---
+++ MicroRabbit.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventHandler<in IEvent>
+        where IEvent : Event
+    {
+        Task Handle(IEvent @event);
+    }
+
+    public interface IEventHandler 
+    {
+
+    }
+
+}
